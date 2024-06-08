7c6a344d50d08a5f84b786ffffd0a68e908ba792
Faizan Ali <fali.abdulali@gmail.com>
2024-06-08 11:34:41 +0500

    Project setup and structuring.

---
+++ components/Nav.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Link from 'next/link'
+import Image from 'next/image'
+
+import { useState, useEffect } from 'react'
+
+const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav
