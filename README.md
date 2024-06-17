5561a575251289dc936c922b6d846484e1b4edf7
Faizan Ali <fali.abdulali@gmail.com>
2024-06-17 14:12:28 +0500

    Tutorial finished.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Tutorial credits go to: [Tutorial Video](https://www.youtube.com/watch?v=wm5gMKuwSYk)
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,5 +1,87 @@
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter, usePathname } from "next/navigation";
+
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { data: session } = useSession();
+
+  const [copied, setCopied] = useState("");
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => {
+      setCopied("");
+    }, 2000);
+  };
+
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex flex-1 justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            alt="user image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+
+        <div onClick={handleCopy} className="copy_btn">
+          <Image
+            src={
+              copied === post.prompt
+                ? "assets/icons/tick.svg"
+                : "assets/icons/copy.svg"
+            }
+            width={12}
+            height={12}
+            alt="copy button"
+          />
+        </div>
+      </div>
+
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+        className="font-inter text-sm blue_gradient cursor-pointer"
+      >
+        {post.tag}
+      </p>
+
+      {session?.user.id === post.creator._id && pathname === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            onClick={handleEdit}
+            className="font-inter text-sm green_gradient cursor-pointer"
+          >
+            Edit
+          </p>
+          <p
+            onClick={handleDelete}
+            className="font-inter text-sm orange_gradient cursor-pointer"
+          >
+            Delete
+          </p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PromptCard;
